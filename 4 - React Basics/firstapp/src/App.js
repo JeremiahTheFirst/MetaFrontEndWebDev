5d1f9f21c0f942d349a1d2210c685ae6d4c45ee4
Jeremiah Adams <jeremiah.d.adams@gmail.com>
2022-12-17 16:00:57 -0600

    Wow so many files

---
+++ 4 - React Basics/firstapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+        <Heading firstName="Bob" />
+        <Heading firstName="Franchesco" />
+      </div>
+  );
+}
+
+export default App;
