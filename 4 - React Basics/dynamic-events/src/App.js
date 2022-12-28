787e62faad201fee45392a1e246332d0a5ce3af5
Jeremiah Adams <jeremiah.d.adams@gmail.com>
2022-12-27 20:40:29 -0600

    Managing-state-in-react starting point

--- 4 - React Basics/dynamic-events/src/App.js
+++ 4 - React Basics/dynamic-events/src/App.js
@@ -1,16 +1,12 @@
-function App() {
-  
-  function handleClick() { 
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number'); 
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
+function App() {
   return (
-    <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits />
+      <FruitsCounter />
     </div>
   );
 }
