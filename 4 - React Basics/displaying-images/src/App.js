3624a984f70e6924f609c1a7e6733dc95e522cdc
Jeremiah Adams <jeremiah.d.adams@gmail.com>
2023-01-02 12:04:14 -0600

    Finished lab plus extra height filter

--- 4 - React Basics/displaying-images/src/App.js
+++ 4 - React Basics/displaying-images/src/App.js
@@ -1,7 +1,14 @@
+import logo from './assets/logo.png';
+
 function App() {
   return (
     <div className="App">
       <h1>Task: Add an image below</h1>
+      <img
+        height = {300}
+        src = {logo}
+        alt = "Logo"
+      />
     </div>
   );
 }
