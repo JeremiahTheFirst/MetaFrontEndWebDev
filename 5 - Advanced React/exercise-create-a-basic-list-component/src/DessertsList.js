cc22edc791f389b16eea979e4ec1ff8392b314a2
Jeremiah Adams <jeremiah.d.adams@gmail.com>
2023-01-24 17:50:54 -0600

    Finished first lab

--- 5 - Advanced React/exercise-create-a-basic-list-component/src/DessertsList.js
+++ 5 - Advanced React/exercise-create-a-basic-list-component/src/DessertsList.js
@@ -1,6 +1,23 @@
 function DessertsList(props) {
   // Implement the component here.
-  return null;
+  const lowCalDessert = props.data
+  .filter((dessert) => {
+    return dessert.calories<500;
+    })
+  .sort((a,b) => {
+    return a.calories - b.calories;
+  })
+ .map((dessert) => {
+    return(
+      <li>
+        {dessert.name} - {dessert.calories} cal
+      </li>
+    );
+ });
+ 
+ return (
+  <ul>{lowCalDessert}</ul>
+ );
 }
 
 export default DessertsList;
